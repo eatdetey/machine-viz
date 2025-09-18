1e74087cddb9d66529b834e3769d1050420ed653
eatdetey <vsevozmoznoe@mail.ru>
2025-09-19 01:47:32 +0300

    L1 T4 Сохранить видео в другой файл

--- main.py
+++ main.py
@@ -16,17 +16,31 @@ import cv2 as cv
 # cv.destroyAllWindows()
 
 cap = cv.VideoCapture("freaky-freak.mp4", cv.CAP_ANY)
+#
+# while (cap.isOpened()):
+#     ret, frame = cap.read()
+#     if ret:
+#         new_size = cv.resize(frame, (600, 200))
+#         cv.imshow("Video Freak", new_size)
+#     else:
+#         cap.set(cv.CAP_PROP_POS_FRAMES, 0)
+#         continue
+#     if cv.waitKey(1) & 0xFF == 27:
+#         break
+#
+# cap.release()
+# cv.destroyAllWindows()
 
-while (cap.isOpened()):
+fps = cap.get(cv.CAP_PROP_FPS)
+width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+fourcc = cv.VideoWriter_fourcc(*'XVID')
+video_writer = cv.VideoWriter("copy_output.mov", fourcc, fps, (width,height))
+while (True):
     ret, frame = cap.read()
-    if ret:
-        new_size = cv.resize(frame, (600, 200))
-        cv.imshow("Video Freak", new_size)
-    else:
-        cap.set(cv.CAP_PROP_POS_FRAMES, 0)
-        continue
-    if cv.waitKey(1) & 0xFF == 27:
+    cv.imshow('Video', frame)
+    video_writer.write(frame)
+    if cv.waitKey(1) & 0xFF == ord('q'):
         break
-
 cap.release()
 cv.destroyAllWindows()
